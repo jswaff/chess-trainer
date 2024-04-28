6865b4471ee57a1652a8ab99b76841a709b59c5e
James Swafford <j.f.swafford@gmail.com>
2024-04-28 08:20:45 -0400

    config

--- config.py
+++ config.py
@@ -2,16 +2,17 @@ import torch
 
 class CFG:
 
-    input_path = 'data/E12.33-1M-D12.csv'
-    model_name = 'models/E12.33-1M-D12.pt'
+    test_name = 'rr3-1c'
+    input_path = 'data/' + test_name + '.csv'
+    model_name = 'models/' + test_name + '.pt'
     num_samples = None
 
     num_features = 768
-    batch_size = 1024
+    batch_size = 512
     num_workers = 8
     num_epochs = 100
     lr = 0.001
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    show_plots = False
+    show_plots = True
