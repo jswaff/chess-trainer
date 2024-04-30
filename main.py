d311652186546e54b9bbea60f93283862036c023
James Swafford <j.f.swafford@gmail.com>
2024-04-30 08:04:35 -0400

    create main() method

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from data import build_data_loaders
 from train import train
 
 
-if __name__ == "__main__":
-    #torch.multiprocessing.set_start_method('spawn')
+def main():
+    # torch.multiprocessing.set_start_method('spawn')
     print("device: ", CFG.device)
-    # build data loaders
+
     train_dl, train_sz, test_dl, test_sz, valid_dl, valid_sz = build_data_loaders()
 
     # define the model
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     torch.manual_seed(1)
     hist = train(model, CFG.num_epochs, train_dl, train_sz, valid_dl, valid_sz, loss_fn, optimizer)
     print(f'Min validation loss: {hist[0]:.4f}')
-    #print(f'Best validation weights: {hist[1]}')
+    # print(f'Best validation weights: {hist[1]}')
 
     # measure performance against test set
     loss_test = 0
@@ -42,7 +42,7 @@ if __name__ == "__main__":
         y_batch = y_batch.squeeze(0).to(CFG.device)
         pred = model(x_batch)
         loss = loss_fn(pred, y_batch)
-        loss_test += loss.item() #* y_batch.size(0)
+        loss_test += loss.item()  # * y_batch.size(0)
         if not traced:
             model.load_state_dict(hist[1])
             traced_script_module = torch.jit.trace(model, x_batch)
@@ -54,9 +54,13 @@ if __name__ == "__main__":
     # visualize learning curves
     if CFG.show_plots:
         x_arr = np.arange(len(hist[2])) + 1
-        fig = plt.figure(figsize=(12,4))
-        ax = fig.add_subplot(1,2,1)
+        fig = plt.figure(figsize=(12, 4))
+        ax = fig.add_subplot(1, 2, 1)
         ax.plot(x_arr, hist[2], '-o', label='Train loss')
         ax.plot(x_arr, hist[3], '--<', label='Validation loss')
         ax.legend(fontsize=15)
         plt.show()
+
+
+if __name__ == "__main__":
+    main()
