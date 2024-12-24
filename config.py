a3124d5191cff31a61afd5e95400ae4053fd1b7e
James Swafford <j.f.swafford@gmail.com>
2024-12-24 09:09:24 -0500

    use 32k batch size.  slight tweak to early stop condition

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from model import Model
 class CFG:
     data_path = '/home/james/data/chess/labeled/positions-d2.csv'
     input_model_name = None #'/home/james/data/chess/models/all-d1.pt'
-    output_model_name = '/home/james/data/chess/models/nn-004-d2.pt'
+    output_model_name = '/home/james/data/chess/models/nn-32k.pt'
 
-    batch_size = 1024
+    batch_size = 1024 * 32
     num_workers = 8
-    num_epochs = 300
+    num_epochs = 100
     lr = 0.001
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
