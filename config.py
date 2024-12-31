9ab27c83dc1bd3e9a042d32ca00208ad8206be65
James Swafford <j.f.swafford@gmail.com>
2024-12-31 05:25:20 -0500

    remove symmetry

--- config.py
+++ config.py
@@ -5,17 +5,18 @@ from model import Model
 class CFG:
     data_path = 'data/labeled/positions-d2.csv'
     input_model_name = None
-    output_model_name = 'models/nn.pt'
+    output_model_name = 'models/nn-d2-lr.pt'
 
     batch_size = 1024 * 32
     num_workers = 8
     num_epochs = 100
-    lr = 0.001
+    lr = 0.0005
+    early_stop_counter = 3
     early_stop_threshold = 0.001
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = Model(input_model_name)
 
-    clear_cache = True
+    clear_cache = False
     show_plots = True
---
+++ convert_cerebrum.py
@@ -0,0 +1,17 @@
+# utility script to convert CSV into Cerebrum format
+
+ifile = "/home/james/data/chess/labeled/positions-d3.csv"
+ofile = "/home/james/data/chess/labeled/positions-d3-cerebrum.csv"
+
+with open(ifile, "r") as infile, open(ofile, "w") as outfile:
+    for line in infile:
+        [score, rest] = line.split(',')
+        epd = rest.split(' ')
+        wscore = int(score)
+        if 'b' == epd[1]:
+            wscore = -wscore
+        elif 'w' != epd[1]:
+            raise Exception("unknown ptm!")
+        if abs(wscore) < 32000:
+            outline = epd[0] + ';' + str(wscore) + ';' + str(-wscore) + '\n'
+            outfile.write(outline)
