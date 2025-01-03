f190e9fad4274bb470569d84937614e44226fe5f
James Swafford <j.f.swafford@gmail.com>
2025-01-03 13:19:39 -0500

    cerebrum scores should be in pawns

--- convert_cerebrum.py
+++ convert_cerebrum.py
@@ -1,13 +1,13 @@
 # utility script to convert CSV into Cerebrum format
 
-ifile = "/home/james/data/chess/labeled/positions-d3.csv"
-ofile = "/home/james/data/chess/labeled/positions-d3-cerebrum.csv"
+ifile = "/home/james/data/chess/labeled/positions.csv"
+ofile = "/home/james/data/chess/labeled/positions-cerebrum.csv"
 
 with open(ifile, "r") as infile, open(ofile, "w") as outfile:
     for line in infile:
         [score, rest] = line.split(',')
         epd = rest.split(' ')
-        wscore = int(score)
+        wscore = float(score) / 100.0
         if 'b' == epd[1]:
             wscore = -wscore
         elif 'w' != epd[1]:
